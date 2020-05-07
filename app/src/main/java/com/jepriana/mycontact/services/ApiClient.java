d8a7b753e3f200c68d4dc8df8bbc5014b0d66ec4
Jepriana <jepriana.id@gmail.com>
2020-05-07 11:13:36 +0800

    Initial commit

---
+++ app/src/main/java/com/jepriana/mycontact/services/ApiClient.java
@@ -0,0 +1,24 @@
+package com.jepriana.mycontact.services;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    public static Retrofit retrofit;
+    public static final String BASE_URL = "https://jepriana.stikom-bali.ac.id/ws/api/";
+    public static final String IMAGE_URL = "https://jepriana.stikom-bali.ac.id/ws/images/";
+
+    public static Retrofit getRetrofitInstance(){
+        if (retrofit==null){
+            try {
+                retrofit = new Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+            } catch (Exception er) {
+                er.printStackTrace();
+            }
+        }
+        return retrofit;
+    }
+}
